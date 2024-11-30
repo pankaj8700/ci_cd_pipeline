aacec6dc54888dd1ee4f0563dcd5dbe81ef6184d
Pankaj8700 <pankajku.8700@gmail.com>
2024-11-30 16:43:11 +0530

    spelling mistake

--- model/train.py
+++ model/train.py
@@ -15,7 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 # Train the RandomForest model
 model = RandomForestClassifier(n_estimators = 100, random_state = 42)
-model.fir(X_train, y_train)
+model.fit(X_train, y_train)
 
 # Save the model
 joblib.dump(model, 'model/iris_model.pkl')
