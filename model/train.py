06b6ea3f9ef9d4fc463d0dbda8a3e0eb4857f468
Pankaj8700 <pankajku.8700@gmail.com>
2024-11-30 17:06:15 +0530

    change in linear model

--- model/train.py
+++ model/train.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import joblib
 
 # Load the dataset
@@ -14,7 +14,7 @@ y = data['species']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
 # Train the RandomForest model
-model = RandomForestClassifier(n_estimators = 100, random_state = 42)
+model = LogisticRegression(max_iter = 200)
 model.fit(X_train, y_train)
 
 # Save the model
--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,7 @@ import unittest
 class TestModel(unittest.TestCase):
     def test_model(self):
         model = joblib.load('model/iris.pkl')
-        self.assertIsInstance(model, RandomForestClassifier)
+        self.assertIsInstance(model, LogisticRegression)
         self.assertGreaterEqual(len(model.feature_importances_), 4)
 
 if __name__ == '__main__':
